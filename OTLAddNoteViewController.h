142617080bb8251556c10d0a6fd0f1c5df087e62
laousyot <othmane.laousy@gmail.com>
2013-03-28 19:40:05 -0400

    core data

---
+++ OTLAddNoteViewController.h
@@ -0,0 +1,13 @@
+//
+//  OTLAddNoteViewController.h
+//  LastNotes
+//
+//  Created by Othmane Laousy on 3/26/13.
+//  Copyright (c) 2013 Othmane Laousy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface OTLAddNoteViewController : UIViewController
+
+@end
---
+++ OTLAddNoteViewController.m
@@ -0,0 +1,38 @@
+//
+//  OTLAddNoteViewController.m
+//  LastNotes
+//
+//  Created by Othmane Laousy on 3/26/13.
+//  Copyright (c) 2013 Othmane Laousy. All rights reserved.
+//
+
+#import "OTLAddNoteViewController.h"
+
+@interface OTLAddNoteViewController ()
+
+@end
+
+@implementation OTLAddNoteViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
